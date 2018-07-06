d7c7f67ee2906ba3e97bc4da2d615c158389cbd6
Jack <jcf9588@rit.edu>
2018-07-06 01:26:32 -0400

    cleaned up code/left middleware template for battleRouter

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -13,23 +13,6 @@ mainRouter.use(session({
   activeDuration: 5 * 60 * 1000,
 }));
 
-mainRouter.use(function (req, res, next) {
-  if (req.session && req.session.user) {
-    User.findOne({ email: req.session.user.email }, function (err, user) {
-      if (user) {
-        req.user = user;
-        delete req.user.password; // delete the password from the session
-        req.session.user = user;  //refresh the session value
-        res.locals.user = user;
-      }
-      // finishing processing the middleware and run the route
-      next();
-    });
-  } else {
-    next();
-  }
-});
-
 // main root for homepage
 mainRouter.get("/", (req, res, next) => {
   // TODO: if session take user to dashboard, if not make them sign in/sign up
